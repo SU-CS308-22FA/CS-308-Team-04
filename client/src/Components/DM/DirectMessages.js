0b290ad95da95ca7d755094fc0fb921c143cd948
MehmetKiraz00 <94314696+MehmetKiraz00@users.noreply.github.com>
2023-01-01 19:40:12 +0300

    Revert "Feature/send message"

--- client/src/Components/DM/DirectMessages.js
+++ client/src/Components/DM/DirectMessages.js
@@ -4,20 +4,11 @@ import Navbar from "../Navigation/navbar";
 import { USE_LOCAL_BACKEND } from "../../config";
 import Card from "../UI/Card/Card";
 import classes from "./DirectMessages.module.css";
-import TextField from '@mui/material/TextField';
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import { padding } from "@mui/system";
-
 const DirectMessages = () => {
-  const [dm_id, setDmId] = React.useState("");
   const location = useLocation();
   const userInfo = location.state.userInfo;
   const my_user_id = location.state.sender_id;
   const other_userid = location.state.receiver_id;
-  const [MessageContent, setMessageContent] = React.useState("");
-  const [isValidMessage, set_isValidMessage] = React.useState(false);
   const [messagesList, setMessagesList] = useState([]);
   const [MyUserInfo, setMyUserInfo] = useState({
     name: "",
@@ -36,47 +27,6 @@ const DirectMessages = () => {
     return isTrue;
   }
 
-  const MessageContentHandler = (event) => {
-    setMessageContent(event.target.value);
-  }
-
-  const MessageSendHandler = (event) => {
-    if (isValidMessage) {     
-      const requestOptions = {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          sender_id: localStorage.getItem("user"),
-          message_content: MessageContent,
-        }),
-      }; 
-      fetch(
-        USE_LOCAL_BACKEND
-          ? `/GencFootball/dm/sendMessage/${dm_id}`
-          : `https://genc-football-backend.herokuapp.com/GencFootball/dm/sendMessage/${dm_id}`,
-        requestOptions
-      )
-        .catch((err) => {
-          console.log("Caught error", err);
-        })
-        .then((response) => response.json());
-      
-      setMessageContent("");
-    }
-    else {
-      console.log("Message content should be valid");
-    }
-  }
-
-  useEffect(() => {
-    if (MessageContent.trim().length === 0) {
-      set_isValidMessage(false);
-    }
-    else {
-      set_isValidMessage(true);
-    }
-  }, [MessageContent, isValidMessage])
-
   useEffect(() => {
     fetch(
       USE_LOCAL_BACKEND
@@ -92,8 +42,6 @@ const DirectMessages = () => {
         console.log("Data is", data);
         console.log("Direct Messages is1 ", data.direct_messages);
         setMessagesList(data.direct_messages);
-        setDmId(data._id);
-        //console.log("DM ID!!!!!:",dm_id);
         console.log("Direct Messages is2 ", messagesList);
         if (my_user_id === data.user_info1._id) {
           console.log("Yes they are equal");
@@ -143,21 +91,6 @@ const DirectMessages = () => {
           );
         })}
       </div>
-      <div className={classes.dm_message_send_bundle}>
-        <div className={classes.dm_message_content_window}>
-          <TextField
-              label="Message content"
-              id="Message-Content"
-              size="small"
-              variant="standard"
-              fullWidth
-              onChange={MessageContentHandler}
-              value={MessageContent}
-            />
-        </div>
-        <Button variant="contained" endIcon={<SendIcon/>} onClick={MessageSendHandler}>Send</Button>
-      </div>
-
     </>
   );
 }
