687425a4a68067f497784206eefba78927f50288
denizumuteser <denizumuteser@yahoo.com.tr>
2023-01-01 14:42:04 +0300

    added 2 unit tests for reporting feature

---
+++ client/tests/test.py
@@ -0,0 +1,95 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:\chromedriver\chromedriver.exe")
+
+    def test_for_reporting_posts(self):
+        driver = self.driver
+        driver.get("http://www.gencfootball.com/login")
+        time.sleep(5)
+        #login
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys('denizumut@sabanciuniv.edu')
+        actions.send_keys(Keys.TAB)
+        actions.send_keys('Password1')
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.ENTER)
+        actions.perform()
+        #moved to profile
+        time.sleep(3)
+        driver.get("http://www.gencfootball.com/Feed")
+        #moved to feed
+        #search for report button
+        time.sleep(3)
+        reportButton = driver.find_element(By.XPATH, '//button[normalize-space()="Report"]')
+        self.assertIn("Report", reportButton.text, "Return button not found in post list")      
+        time.sleep(3)
+        #click report button
+        reportButton.click()
+        #alert = driver.switch_to.alert()
+        alert = WebDriverWait(driver, 5).until(EC.alert_is_present())
+        time.sleep(1)
+        self.assertIn("Success", alert.text, "Report mail not send.")
+        alert.accept()
+        #closed alert box
+        time.sleep(1)
+        
+    def test_search_reporting_users(self):
+        driver = self.driver
+        driver.get("http://www.gencfootball.com/login")
+        time.sleep(5)
+        
+        #login
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys('denizumut@sabanciuniv.edu')
+        actions.send_keys(Keys.TAB)
+        actions.send_keys('Password1')
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.ENTER)
+        actions.perform()
+        #moved to profile
+        
+        time.sleep(3)
+        #move to another profile in order to report him
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys(Keys.TAB)
+        actions.send_keys('kaportacikoray12')
+        actions.perform()
+        time.sleep(2)
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.DOWN)
+        actions.send_keys(Keys.ENTER)
+        actions.perform()
+        time.sleep(3)
+        reportButton = driver.find_element(By.XPATH, '//button[normalize-space()="Report"]')
+        self.assertIn("Report", reportButton.text, "Return button not found in profile")   
+        time.sleep(3)
+        #click report button
+        reportButton.click()
+        #alert = driver.switch_to.alert()
+        alert = WebDriverWait(driver, 5).until(EC.alert_is_present())
+        time.sleep(1)
+        self.assertIn("Success", alert.text, "Report mail not send.")
+        alert.accept()
+        #closed alert box
+        time.sleep(1)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
